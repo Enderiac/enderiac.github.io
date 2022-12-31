723173eeb09c7a4a5ec17e0b2975d644faaab23e
Enderiac <110489436+Enderiac@users.noreply.github.com>
2022-12-31 13:05:41 -0700

    Update dev.js

--- dev.js
+++ dev.js
@@ -1,1 +1,1 @@
-windows.alert("This website is currently in development and not finished. You can visit my older website at sites.google.com/view/enderiac-hub");
+window.alert("This website is currently in development and not finished. You can visit my older website at sites.google.com/view/enderiac-hub");
