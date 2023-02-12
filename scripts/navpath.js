6d08f7b8dbb51aa949724d3d4e72309c8bc3ff9c
Enderiac <110489436+Enderiac@users.noreply.github.com>
2023-02-11 17:00:23 -0700

    Add files via upload

---
+++ scripts/navpath.js
@@ -0,0 +1,11 @@
+// Sets the paths for navigation bars.
+
+let homepath = "/home"
+let musicpath = "/music"
+let exclusivepath = "/exclusive"
+let socialpath = "https://linktr.ee/enderiac"
+
+document.getElementById("home").href = homepath;
+document.getElementById("music").href = musicpath;
+document.getElementById("exclusive").href = exclusivepath;
+document.getElementById("social").href = socialpath;
